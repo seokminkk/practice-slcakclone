36e732509c139355ec01b428520776401bd4709c
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-11-04 05:18:29 +0900

    페이지 라우터설정

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>초기 세팅입니다.언제까지세팅</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
