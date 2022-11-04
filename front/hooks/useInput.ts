83c4854a424009c105bf6be7b4f5f980b53ae28d
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-11-05 02:55:20 +0900

    회원가입 input 커스텀훅제작

---
+++ front/hooks/useInput.ts
@@ -0,0 +1,15 @@
+import { ChangeEvent, Dispatch, SetStateAction, useCallback, useState } from 'react';
+
+type Handler = (e: ChangeEvent<HTMLInputElement>) => void; //애니를안쓰고싶으면
+// type Handler = (e: any) => void;
+type ReturnTypes<T = any> = [T, Handler, Dispatch<SetStateAction<T>>];
+const useInput = <T = any>(initialValue: T): ReturnTypes<T> => {
+  const [value, setValue] = useState(initialValue);
+  const handler = useCallback((e) => {
+    setValue(e.target.value as unknown as T);
+    // setValue(e.target.value);
+  }, []);
+  return [value, handler, setValue];
+};
+
+export default useInput;
