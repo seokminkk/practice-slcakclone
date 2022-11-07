5449528f35e35c40af744c4b3899b53d6136ef89
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-11-07 11:38:41 +0900

    채널페이지 생성

---
+++ front/pages/Channel/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Workspace from '@layouts/Workspace';
+
+const Channel = () => {
+  return (
+    <Workspace>
+      <div>록인하신 것을 축하드려요!</div>
+    </Workspace>
+  );
+};
+export default Channel;
