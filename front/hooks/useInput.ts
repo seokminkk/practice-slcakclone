98f50b54723ba6dcf428e24ee803c0a2d48d499d
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-11-09 03:43:38 +0900

    타입any 수정

--- front/hooks/useInput.ts
+++ front/hooks/useInput.ts
@@ -3,7 +3,7 @@ import { ChangeEvent, Dispatch, SetStateAction, useCallback, useState } from 're
 type Handler = (e: ChangeEvent<HTMLInputElement>) => void; //애니를안쓰고싶으면
 // type Handler = (e: any) => void;
 type ReturnTypes<T> = [T, Handler, Dispatch<SetStateAction<T>>];
-const useInput = <T extends string | number>(initialValue: T): ReturnTypes<T> => {
+const useInput = <T>(initialValue: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialValue);
   const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value as unknown as T);
